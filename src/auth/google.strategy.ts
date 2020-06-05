32d7974fa436171172e99b97e32d7bce6a803a11
Sarim Abbas <sarim.stays@gmail.com>
2020-06-05 14:00:52 -0400

    conditional base URL for google auth

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -3,13 +3,17 @@ import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(private configService: ConfigService) {
     super({
       clientID: configService.get<string>('AUTH_GOOGLE_CLIENT_ID'),
       clientSecret: configService.get<string>('AUTH_GOOGLE_CLIENT_SECRET'),
-      callbackURL: 'http://localhost:3000/auth/google/redirect',
+      callbackURL: isProd
+        ? `${configService.get<string>('PROD_BASE_URL')}/auth/google/redirect`
+        : `${configService.get<string>('DEV_BASE_URL')}/auth/google/redirect`,
       scope: ['email', 'profile'],
     });
   }
