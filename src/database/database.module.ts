2919f3179a9009f91ed6335cab6d43c3800aa4b2
Sarim Abbas <sarim.stays@gmail.com>
2020-06-04 22:04:10 -0400

    add database_url setting to database config for prod environments

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -6,20 +6,36 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 // Our logic
 import { DatabaseService } from './database.service';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DATABASE_HOST'),
-        port: +configService.get<number>('DATABASE_PORT'),
-        username: configService.get('DATABASE_USERNAME'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
-        synchronize: configService.get<boolean>('DATABASE_SYNCHRONIZE', false),
-        autoLoadEntities: true,
-        // entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      }),
+      useFactory: (configService: ConfigService) => {
+        return isProd
+          ? {
+              url: configService.get('DATABASE_URL'),
+              type: 'postgres',
+              synchronize: configService.get<boolean>(
+                'DATABASE_SYNCHRONIZE',
+                false,
+              ),
+              autoLoadEntities: true,
+            }
+          : {
+              host: configService.get('DATABASE_HOST'),
+              port: +configService.get<number>('DATABASE_PORT'),
+              username: configService.get('DATABASE_USERNAME'),
+              password: configService.get('DATABASE_PASSWORD'),
+              database: configService.get('DATABASE_NAME'),
+              type: 'postgres',
+              synchronize: configService.get<boolean>(
+                'DATABASE_SYNCHRONIZE',
+                false,
+              ),
+              autoLoadEntities: true,
+            };
+      },
       inject: [ConfigService],
     }),
   ],
