e0764ddbc9f7249ef9871dac14b126ca345f0894
Sarim Abbas <sarim.stays@gmail.com>
2020-06-10 12:44:59 -0400

    add documentation to auth module. convert the utilities into a reusable service

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -3,6 +3,10 @@ import * as jwt from 'express-jwt';
 import { expressJwtSecret } from 'jwks-rsa';
 import { Request, Response } from 'express';
 
+// source:
+// https://github.com/bipiane/nest-react-auth0-blog/blob/master/blog-api/src/common/authentication.middleware.ts
+// this middleware verifies incoming JWT tokens against the Auth0 service. Only
+// when the token is deemed valid does the endpoint become accessible
 @Injectable()
 export class JWTMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,20 +5,34 @@ import {
   RequestMethod,
 } from '@nestjs/common';
 
+// AuthController: all our endpoints
 import { AuthController } from './auth.controller';
+
+// AuthService: reusable logic lives here
+import { AuthService } from './auth.service';
+
+// JWTMiddleware: protects endpoints with JWT auth
 import { JWTMiddleware } from './auth.middleware';
 
 @Module({
+  // module imports
   imports: [],
-  providers: [],
+
+  // service imports
+  providers: [AuthService],
+
+  // controller imports
   controllers: [AuthController],
-  exports: [],
+
+  // re-exports for other modules' usage
+  exports: [AuthService],
 })
 export class AuthModule implements NestModule {
+  // protect certain routes with JWT auth
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(JWTMiddleware).forRoutes({
-      path: 'auth/test',
-      method: RequestMethod.ALL,
+      path: 'auth/test', // the path to the route we want to protect
+      method: RequestMethod.ALL, // the method e.g. GET, POST or ALL
     });
   }
 }
