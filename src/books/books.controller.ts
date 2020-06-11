1962a9383e4b7d45dcfcd71d495fb92cf4ceab98
Felicia Chang <felicia.chang@yale.edu>
2020-06-10 20:20:01 -0700

    book

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -11,35 +11,40 @@ export class BooksController {
     private readonly booksRepository: Repository<BookEntity>,
   ) {}
 
-  @Get()
-  getBooks(@Param() params): Promise<any> {
-    // return this.booksRepository
-    //   .createQueryBuilder('book')
-    //   .leftJoinAndSelect('book.reviews', 'review')
-    //   .addSelect('Count(review.id)', 'reviewCount')
-    //   .groupBy('book.id, review.id')
-    //   .orderBy({
-    //     reviewCount: 'DESC',
-    //   })
-    //   .getMany();
+  // @Get()
+  // getBooks(@Param() params): Promise<any> {
+  //   // return this.booksRepository
+  //   //   .createQueryBuilder('book')
+  //   //   .leftJoinAndSelect('book.reviews', 'review')
+  //   //   .addSelect('Count(review.id)', 'reviewCount')
+  //   //   .groupBy('book.id, review.id')
+  //   //   .orderBy({
+  //   //     reviewCount: 'DESC',
+  //   //   })
+  //   //   .getMany();
 
-    return this.booksRepository.query(`SELECT b.id, b.name FROM book b
-    LEFT OUTER JOIN book_review r ON b.id = r."bookId"
-    GROUP BY b.id, b.name ORDER BY Count(r."bookId") DESC`);
+  //   return this.booksRepository.query(`SELECT b.id, b.name FROM book b
+  //   LEFT OUTER JOIN book_review r ON b.id = r."bookId"
+  //   GROUP BY b.id, b.name ORDER BY Count(r."bookId") DESC`);
+  // }
+
+  @Post('getonebook')
+  async getOneBook(@Body() body): Promise<any> {
+    let check = await this.booksRepository.findOne({
+      where: {
+        isbn: body.isbn,
+      },
+    });
+    return check;
   }
 
-  @Get()
-  getOneBook(@Param() params): Promise<any> {
-    return this.booksRepository.find();
-  }
-
+  //how to make an array of suthors
   @Post()
   createBook(@Body() body: CreateBookDTO): Promise<BookEntity> {
     const book = new BookEntity();
     book.name = body.name;
     book.author = body.author;
     book.isbn = body.isbn;
-    book.googleId = body.googleId;
     return this.booksRepository.save(book);
   }
 }
